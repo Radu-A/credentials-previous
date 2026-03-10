f26b238ccc0bec516bddcb66c2a7bf86eea3452f
Radu-A <elcastravete@gmail.com>
2026-03-10 23:25:17 +0100

    fix(config): change ts config and imports

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,9 @@ import router from "./routes";
 const app = express();
 app.use(express.json());
 
-// Enganchamos el router a la ruta base '/sync'
 app.use("/sync", router);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 MVP modularizado listo y escuchando en el puerto ${PORT}`);
+  console.log(`App ready and listening in "http://localhost:${PORT}"`);
 });
